f96e813efbade44121e42215bf671b4be1b6578f
James Pow <jbpow18@icloud.com>
2023-02-05 15:03:35 -0500

    First commit.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SQL Practice</title>
+  <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+  <header>
+    <h1>Practice Your SQL!</h1>
+  </header>
+  <main>
+    <form>
+      <input type="text" name="query" placeholder="SELECT * FROM <tablename> WHERE ...">
+      <input type="submit" value="submit">
+    </form>
+  </main>
+  <footer>
+    <p>Contact us at sqlPractice@sqlP.com</p>
+    <p>Copyright &copy 2023 sqlstuff</p>
+  </footer>
+</body>
+
+</html>
