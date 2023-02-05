97a431fc617f1c4dd41a77632bc5dfcf374e7a89
James Pow <jbpow18@icloud.com>
2023-02-05 17:13:22 -0500

    Filled table showinfo in phishdb. Had to skip shows with apostrophe to avoid PDOException error.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+$query = filter_input(INPUT_POST, "query", FILTER_SANITIZE_SPECIAL_CHARS);
+//echo $query;
+include_once "./config/Database.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,10 +21,27 @@
     <h1>Practice Your SQL!</h1>
   </header>
   <main>
-    <form>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
       <input type="text" name="query" placeholder="SELECT * FROM <tablename> WHERE ...">
       <input type="submit" value="submit">
+      <input type="submit" value="reset" onclick="window.location.href='index.php'">
     </form>
+    <div class="output">
+      <?php
+        include_once "./config/phishapi.php";
+        echo $query;
+        for ($i = 0; $i < count($data["data"]); $i++) {
+          // skip show with apostrophe in venue name
+          if (strpos($data['data'][$i]['venue'], "'") !== false) {
+            continue;
+          }
+        $stmt = $pdo->prepare("INSERT INTO `showinfo` (`date`, `state`, `city`, `venue`) VALUES ('{$data["data"][$i]["showdate"]}', '{$data["data"][$i]["state"]}', '{$data["data"][$i]["city"]}', '{$data["data"][$i]["venue"]}')");
+          $stmt->execute();
+          $stmt->closeCursor();
+        }
+
+      ?>
+    </div>
   </main>
   <footer>
     <p>Contact us at sqlPractice@sqlP.com</p>
